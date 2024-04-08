b4bb1b2a7c076621354f71fadf7630839a5ac264
keith-kay <keithrhova@gmail.com>
2024-04-08 12:08:34 +0300

    meal selection logic update commit

--- canteen_system/app/Http/Controllers/MealSelectionController.php
+++ canteen_system/app/Http/Controllers/MealSelectionController.php
@@ -1,6 +1,8 @@
 <?php
 
+
 namespace App\Http\Controllers;
+
 
 use App\Models\CustomUser;
 use Carbon\Carbon;
@@ -9,6 +11,7 @@ use App\Models\Logs;
 use Illuminate\Support\Facades\Auth;
 use App\Models\MealType;
 use App\Models\User;
+
 
 class MealSelectionController extends Controller
 {
@@ -19,21 +22,28 @@ class MealSelectionController extends Controller
             'meal_type_id' => 'required|exists:bsl_cmn_mealtypes,bsl_cmn_mealtypes_id',
         ]);
 
+
         // Get the current user's ID
         $userId = auth()->id();
+
 
         // Get the current time
         $current_time = Carbon::now();
 
+
         // start of the current day at 7am
         $shiftStart = Carbon::now()->startOfDay()->setTime(7, 0, 0);
+
 
         // Calculate the start of the next day at 7am
         $nextDayStart = $shiftStart->copy()->addDay();
 
+
         if ($current_time->hour < 7) {
             $shiftStart->subDay();
         }
+
+
 
 
         // Check if the user has already made a meal selection between 7am to 7am of the next day
@@ -42,10 +52,12 @@ class MealSelectionController extends Controller
             ->whereBetween('created_at', [$shiftStart, $nextDayStart]) // Check within the current day from 7am to the next day at 7am
             ->exists();
 
+
         // If a previous entry exists within the specified time frame, prevent the user from making a new entry
         if ($lastEntry) {
             return redirect('/dashboard')->with('error', 'You have already made a meal selection in your shift. Please try again later.');
         }
+
 
         // Create a new Logs entry
         $log = new Logs();
@@ -54,20 +66,25 @@ class MealSelectionController extends Controller
         $log->bsl_cmn_logs_time = now();
         $log->save();
 
+
         // Fetch the latest log entry for the selected meal type
         $latestLog = Logs::where('bsl_cmn_logs_person', $userId)
             ->where('bsl_cmn_logs_mealtype', $validatedData['meal_type_id'])
             ->latest()
             ->first();
 
+
         // Fetch the related user details
         $user = CustomUser::find($userId);
+
 
         // Fetch the related meal type details
         $mealType = MealType::find($validatedData['meal_type_id']);
 
+
         // Format the log time in the Nairobi timezone
         $logTime = Carbon::parse($latestLog->bsl_cmn_logs_time)->timezone('Africa/Nairobi')->format('d/m/Y H:i:s');
+
 
         // Prepare the data for printing
         $data = [
@@ -78,11 +95,12 @@ class MealSelectionController extends Controller
             'date' => $logTime,
         ];
 
+
         //echo json_encode($data);
         //echo $userId;
         // Use the data to print the log
         $curl = curl_init();
-        $url = 'http://api.bulkstream.com:1416/mealprint.php?userid' . $userId . '%printerid=TCMEAL';
+        $url = 'http://api.bulkstream.com:1416/mealprint.php?userid=' . $userId . '&printerid=TCMEAL';
         curl_setopt_array($curl, array(
             CURLOPT_URL => $url,
             CURLOPT_RETURNTRANSFER => true,
@@ -98,17 +116,23 @@ class MealSelectionController extends Controller
             ),
         ));
 
+
         $response = curl_exec($curl);
         if ($response === false) {
             echo "Error: Failed to connect to API.";
-        } else {
-            echo $response;
         }
+        // else {
+        //     echo $response;
+        //     echo $url;
+        //     echo json_encode($data);
+        // }
+
 
         curl_close($curl);
-        echo $response;
+        //echo $response;
+
 
         // Return a success response
-        //return redirect('/dashboard')->with('success', 'Meal selection logged successfully!');
+        return redirect('/dashboard')->with('success', 'Meal selection logged successfully!');
     }
 }
